512f7e93054ff355514bddce8032c81e52a22178
Valentina BM <valentina15bermo@gmail.com>
2026-08-05 21:32:27 -0400

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header.jsx'
+import Content from './components/Content.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
  
   return (
     <>
     <Header />
+    <Content color="red" text="This is my first React Application!"/>
+    <Content color="yellow" text="I am learning React!"/>
+    <Content color="purple" text="React is pretty cool!"/>
+    <Footer />
     </>
   )
 }
 
-export default App
+export default App;
